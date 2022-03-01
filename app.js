95da398cfda7d7f25f3d4a9e0b3abe1def3e27bf
lakshanlach <97869720+lakshanlach@users.noreply.github.com>
2022-03-01 18:15:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+function youtube() {
+    window.location = "https://www.youtube.com/"
+}
+
+function mp4() {
+    window.location = "https://320ytmp3.com/en31/youtube-to-mp4"
+}
+
+function wiki() {
+    window.location = "https://www.wikipedia.org/"
+}
+
+function WHATSAPP() {
+    window.location = "https://www.whatsapp.com/"
+}
+
+function FACEBOOK() {
+    window.location = "https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804791%7Ce%7Cfacebook%7C&placement=&creative=550525804791&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-1001394929%26loc_physical_ms%3D9050528%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiAosmPBhCPARIsAHOen-PCEnb0WkvyVf3VyRkgbrVgrQYl6N8TY9MGWs__mF48ErIkymf7vUwaApDqEALw_wcB"
+}
+
+function ZOOM() {
+    window.location = "https://zoom.us/"
+}
+
+function SNAPCHAT() {
+    window.location = "https://www.snapchat.com/"
+}
+
+function GOOGLEMEET() {
+    window.location = "https://workspace.google.com/intl/en_in/products/meet/?utm_source=google&utm_medium=cpc&utm_campaign=1011352-Workspace-APAC-IN-en-BKWS-EXA-Golden&utm_content=text-ad-none-none-DEV_c-CRE_469259700156-ADGP_Hybrid%20%7C%20AW%20SEM%20%7C%20BKWS%20~%20EXA%20%7C%20Meet%20%7C%20%5BM:1%5D%20%7C%20IN%20%7C%20EN-KWID_43700057528828213-kwd-299356619278&userloc_9050528-network_g=&utm_term=KW_google%20meet&gclid=Cj0KCQiAosmPBhCPARIsAHOen-NG2SJJvSXBw78lFCSeBI9zMy-8abF9u7DVbTY3dTOCp6qMuHY7vdoaAsJAEALw_wcB&gclsrc=aw.ds"
+}
+
+function NETFLIX() {
+    window.location = "https://www.netflix.com/in/"
+}
+
+function DH() {
+    window.location = "https://www.hotstar.com/in"
+}
+
+function NHSCOVID() {
+    window.location = "https://www.nhs.uk/conditions/coronavirus-covid-19/"
+}
+
+function PINTERST() {
+    window.location = "https://in.pinterest.com/"
+}
+
+function TWITTER() {
+    window.location = "https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D"
+}
+
+function MTT() {
+    window.location = "https://www.mxtakatak.com/"
+}
+
+function JOSHVIDEOS() {
+    window.location = "https://myjosh.in/"
+}
+
+function MOJ() {
+    window.location = "https://mojapp.in/"
+}
---
+++ second.js
@@ -0,0 +1,6 @@
+function GAME() {
+    window.location = "game.html"
+}
+function apps() {
+    window.location = "app.html"
+}
